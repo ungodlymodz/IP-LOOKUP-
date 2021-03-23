d3f286c58c230a8729440acbd5ff961bb3659014
ungodlymodz <81203059+ungodlymodz@users.noreply.github.com>
2021-03-22 22:04:51 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,5 +1,8 @@
 apt install python -y
 apt install python2 -y
 apt install ruby -y
+gem install lolcat
 echo '
-Welcome to IP-Lookup made by ungodlymodz 
+ ==============================================
+|         Now Type python3 ipinfo.py           |
+ =============================================='
